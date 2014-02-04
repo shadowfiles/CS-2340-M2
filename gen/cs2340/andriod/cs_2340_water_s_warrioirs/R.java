e06afb6ee78459e8c873f54357ceeb0373b89756
Keith-Cavanaugh <kcavanaugh3@gatech.edu>
2014-02-04 17:34:40 -0500

    started the login page
    it’s userlogin.xml

--- gen/cs2340/andriod/cs_2340_water_s_warrioirs/R.java
+++ gen/cs2340/andriod/cs_2340_water_s_warrioirs/R.java
@@ -26,11 +26,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class id {
         public static final int dummy_button=0x7f070002;
+        public static final int editText1=0x7f070003;
+        public static final int editText2=0x7f070004;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int userlogin=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
