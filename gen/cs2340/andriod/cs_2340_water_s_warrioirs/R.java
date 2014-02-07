00ee7a66264f7a32cd0956739c6fce511aeb1977
Daniel Shea <sadsheady@gmail.com>
2014-02-07 03:33:08 -0500

    Made Welcome Screen Look Pretty
    Changed the background color and added a image.
    I got a weird file into the drawable that I cannot get out

--- gen/cs2340/andriod/cs_2340_water_s_warrioirs/R.java
+++ gen/cs2340/andriod/cs_2340_water_s_warrioirs/R.java
@@ -19,22 +19,25 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
+        public static final int background_standard=0x7f040001;
         public static final int black_overlay=0x7f040000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int money=0x7f020001;
     }
     public static final class id {
         public static final int Register=0x7f080002;
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_forgot_password=0x7f08000a;
-        public static final int email=0x7f080007;
+        public static final int action_forgot_password=0x7f08000b;
+        public static final int email=0x7f080008;
+        public static final int imageView1=0x7f080004;
         public static final int login=0x7f080003;
-        public static final int login_form=0x7f080006;
-        public static final int login_status=0x7f080004;
-        public static final int login_status_message=0x7f080005;
-        public static final int password=0x7f080008;
-        public static final int sign_in_button=0x7f080009;
+        public static final int login_form=0x7f080007;
+        public static final int login_status=0x7f080005;
+        public static final int login_status_message=0x7f080006;
+        public static final int password=0x7f080009;
+        public static final int sign_in_button=0x7f08000a;
         public static final int textView1=0x7f080001;
     }
     public static final class layout {
