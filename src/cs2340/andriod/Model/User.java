eeca7ae71c32b64e02f9ac7882e495660bef4fcb
Qian Liu <qianliu@gatech.edu>
2014-02-16 17:00:57 -0500

    .

--- src/cs2340/andriod/Model/User.java
+++ src/cs2340/andriod/Model/User.java
@@ -5,9 +5,9 @@ public class User {
 	private String username;
 	private String password;
 	
-	public User(String usrnm, String pass) {
-		username = usrnm;
-		password = pass;
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
 	}
 	
 	public String getPassword() {
