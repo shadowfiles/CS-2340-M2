bf274fb5fa0bcc07cedd2ab635daeba6237ef10c
Qian Liu <qianliu@gatech.edu>
2014-02-27 18:14:14 -0500

    test

--- gen/cs2340/andriod/cs_2340_water_s_warriors/R.java
+++ gen/cs2340/andriod/cs_2340_water_s_warriors/R.java
@@ -66,11 +66,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_login=0x7f030002;
         public static final int activity_register=0x7f030003;
         public static final int activity_sucess=0x7f030004;
+        public static final int activity_user_page=0x7f030005;
     }
     public static final class menu {
         public static final int add_acount=0x7f080000;
         public static final int login=0x7f080001;
         public static final int sucess=0x7f080002;
+        public static final int user_page=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f060007;
@@ -91,6 +93,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int title_activity_register=0x7f06000f;
         public static final int title_activity_registration=0x7f06000c;
         public static final int title_activity_sucess=0x7f060006;
+        public static final int title_activity_user_page=0x7f060013;
         public static final int username_text=0x7f060004;
     }
     public static final class style {
