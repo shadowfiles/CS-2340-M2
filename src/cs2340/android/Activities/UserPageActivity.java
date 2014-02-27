c7fc826f4a06c6f2ee6bdf33ecfb819e78b2879b
Qian Liu <qianliu@gatech.edu>
2014-02-27 16:56:05 -0500

    create blank activity

--- src/cs2340/android/Activities/UserPageActivity.java
+++ src/cs2340/android/Activities/UserPageActivity.java
@@ -1,7 +1,39 @@
 package cs2340.android.Activities;
 
+import cs2340.android.Presenters.PresenterInterface;
+import cs2340.android.Views.UserPageView;
 import android.app.Activity;
 
-public class UserPageActivity extends Activity{
+public class UserPageActivity extends Activity implements UserPageView{
+
+	@Override
+	public void attemptUserCallback(PresenterInterface lsnr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void AddAccountButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void LogoutButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void goToAddAccount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void goToIntro() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
