13070c9c9e2074ff1efff9ec6e4caed291b45c3e
Keith-Cavanaugh <kcavanaugh3@gatech.edu>
2014-02-27 12:23:32 -0500

    Coded UserPagePresenter
    also added a method to the UserPageView interface to return to the
    intro screen.

--- src/cs2340/android/Presenters/UserPagePresenter.java
+++ src/cs2340/android/Presenters/UserPagePresenter.java
@@ -1,17 +1,22 @@
 package cs2340.android.Presenters;
+import cs2340.android.Views.UserPageView;
 
 public class UserPagePresenter implements PresenterInterface {
+	
+	private UserPageView view;
+	
+	public UserPagePresenter (UserPageView v) {
+		view = v;
+	}
 
 	@Override
 	public void onClickOne() {
-		// TODO Auto-generated method stub
-		
+		view.goToAddAccount();
 	}
 
 	@Override
 	public void onClickTwo() {
-		// TODO Auto-generated method stub
-		
+		view.LogoutButton();
+		view.goToIntro();
 	}
-
 }
