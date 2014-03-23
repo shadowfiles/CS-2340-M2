2b2700b2851c75bd050c20b852b96056111c7d83
Qian Liu <qianliu@gatech.edu>
2014-03-23 16:10:00 -0400

    fix

--- src/cs2340/android/Activities/SuccessActivity.java
+++ src/cs2340/android/Activities/SuccessActivity.java
@@ -10,7 +10,7 @@ public class SuccessActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sucess);
+		setContentView(R.layout.activity_success);
 	}
 
 	@Override
