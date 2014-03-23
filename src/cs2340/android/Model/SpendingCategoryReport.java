98d32a49a3e30cd89c915f17f87c3a6bfcbccf42
Qian Liu <qianliu@gatech.edu>
2014-03-23 15:46:04 -0400

    add report model

--- src/cs2340/android/Model/SpendingCategoryReport.java
+++ src/cs2340/android/Model/SpendingCategoryReport.java
@@ -3,15 +3,16 @@ package cs2340.android.Model;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class ReportGeneration {
+public class SpendingCategoryReport implements ReportModel {
 	
 	private Collection<TransactionInterface> transactions = new ArrayList<TransactionInterface>();
 	private double totalSpending = 0;
 
-    public ReportGeneration(Collection<TransactionInterface> transactions){
+    public SpendingCategoryReport(Collection<TransactionInterface> transactions){
     	this.transactions = transactions;
     }
     
+    @Override
     public String toString(){
     	String report = "Spending Category Report" + "\n";
     	for (TransactionInterface category : transactions) {
